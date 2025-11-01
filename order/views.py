c4d33290b66b2cd554646cfbd4db8463ae1488f6
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2025-11-01 09:32:55 +0600

    update cartviewset01

--- order/views.py
+++ order/views.py
@@ -25,14 +25,16 @@ class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, Gener
         return Cart.objects.prefetch_related('items__product').filter(user=self.request.user)
     
     def create(self, request, *args, **kwargs):
-        
-        existing_cart = Cart.objects.filter(user=self.request.user).first();
+        # Check if the user already has an active cart
+        existing_cart = Cart.objects.filter(user=request.user).first()
 
         if existing_cart:
-            serializer = self.get_serializer(existing_cart);
-            return Response(serializer.data, status=status.HTTP_200_OK);
-        
-        return super().create(request,*args,**kwargs);
+            # Return the existing cart if it exists
+            serializer = self.get_serializer(existing_cart)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # Otherwise, proceed with creating a new cart
+        return super().create(request, *args, **kwargs)
 
 class CartItemViewSet(ModelViewSet):
     # queryset = CartItem.objects.all()
